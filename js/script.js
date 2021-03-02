1788a7d0fabd07e66dff1ef4e6dffadfe3f4d095
mbagolor <mbagolor@hawaii.edu>
2021-03-02 02:15:59 -0800

    editing

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// MENU BUTTON
+const mymenubutton = document.querySelector(".menu-button");
+const mysitenav = document.querySelector(".site-nav");
+mymenubutton.onclick = function () {
+  if (mysitenav.getAttribute("data-navstate") === "open") {
+    mysitenav.setAttribute("data-navstate", "closed");
+  } else {
+    mysitenav.setAttribute("data-navstate", "open");
+  }
+};
